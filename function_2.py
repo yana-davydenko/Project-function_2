8ef6da13fa1bb3b12b404a40ed56c49141869ee1
iana-davydenko <iana.lady1302@gmail.com>
2020-10-13 01:21:58 +0300

    - a change in the second print: [b-1]

--- function_2.py
+++ function_2.py
@@ -3,8 +3,7 @@ import random
 def function(mylist, a, b):
     number = random.choices(mylist, k = a)
     print(number)
-    c = b-1
-    print(number[c]) #print the forth digit in the new list
+    print(number[b-1]) #print the forth digit in the new list
 
 function([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 18, 4)
 
